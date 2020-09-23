80c4268edf9d934e0bab54097e2f7899e2708566
Jordis Man <jordisman@gmail.com>
2020-09-23 14:15:46 -0700

    Created navbar and header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from './Navbar';
+import styles from '../styles.module.css';
+
+function Header() {
+  return (
+
+      <div className={styles.headerContainer}>
+        <Navbar />
+        <div className={styles.titleBox}>
+          <h1 className={styles.title}>
+            <span className={styles.titleMain}>Jordis Man</span>
+          </h1>
+          <p className={styles.titleSub}>
+            Software Engineer based in San Francisco
+          </p>
+
+        </div>
+      </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles.module.css';
+
+const Navbar = () => {
+  const underline = {textDecoration: "none"};
+  const textColor = {color: "#F55E61"};
+
+  return (
+    <div className={styles.navbar}>
+      <ul className={styles.navUl}>
+        <li className={styles.navLi}>Home</li>
+        <li className={styles.navLi}>About</li>
+        <li className={`${styles.navLi} ${styles.signUp}`}>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
