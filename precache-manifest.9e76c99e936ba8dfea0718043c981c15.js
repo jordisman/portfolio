5498b08448c97cd5fb1a25450d4924276da69db8
Jordis Man <jordisman@gmail.com>
2020-10-09 15:45:28 -0700

    Updates

---
+++ precache-manifest.9e76c99e936ba8dfea0718043c981c15.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bad49bebf23f00868e9e0b1c41ac14",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cb798231b5e23938f66c",
+    "url": "/portfolio/static/css/main.f94b464b.chunk.css"
+  },
+  {
+    "revision": "ab534edff4743a96a67f",
+    "url": "/portfolio/static/js/2.d3d63a17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d3d63a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb798231b5e23938f66c",
+    "url": "/portfolio/static/js/main.122733c9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "966fe1b3a1898383c69eb573458b7ef4",
+    "url": "/portfolio/static/media/mountain-switzerland.966fe1b3.jpg"
+  },
+  {
+    "revision": "d6c15bf0efcaf4c03c5cb0c9078cdd17",
+    "url": "/portfolio/static/media/nat-1.d6c15bf0.jpg"
+  },
+  {
+    "revision": "4d3783688636b59662283e19991a2912",
+    "url": "/portfolio/static/media/nat-2.4d378368.jpg"
+  },
+  {
+    "revision": "cc2b05433cbad0b369d131d02c0cac6a",
+    "url": "/portfolio/static/media/nat-3.cc2b0543.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/portfolio/static/media/nat-6.1ef6d764.jpg"
+  }
+]);
